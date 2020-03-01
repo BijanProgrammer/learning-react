2f44c3be2e7f9ddfa8a4b52ed6229eff1650537b
BijanEisapour <bijaneisapour@gmail.com>
2020-03-01 21:25:46 +0330

    Added basic components: Login, Register

---
+++ src/components/login/register.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import loginImage from '../../login.svg';
+
+export class Register extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="base-container">
+				<div className="head">وارد شوید!</div>
+
+				<div className="content">
+					<div className="image">
+						<img src={loginImage} alt="Login" />
+					</div>
+
+					<div className="form">
+						<div className="form-group">
+							<label htmlFor="username">نام کاربری</label>
+							<input type="text" name="username" placeholder="نام کاربری" />
+						</div>
+						<div className="form-group">
+							<label htmlFor="email">ایمیل</label>
+							<input type="text" name="email" placeholder="ایمیل" />
+						</div>
+						<div className="form-group">
+							<label htmlFor="password">رمز عبور</label>
+							<input type="text" name="password" placeholder="رمز عبور" />
+						</div>
+					</div>
+				</div>
+
+				<div className="footer">
+					<button className="btn" type="button">
+						ورود
+					</button>
+				</div>
+			</div>
+		);
+	}
+}
